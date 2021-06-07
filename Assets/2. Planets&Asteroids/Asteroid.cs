9f351b2901775df4bba0a026f083fc18ea689bb6
Francesco Avitabile <78053731+55hp@users.noreply.github.com>
2021-06-07 17:29:20 +0200

    Initial commit

---
+++ Assets/2. Planets&Asteroids/Asteroid.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(1, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * (speed/2) * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Deadzone")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/2. Planets&Asteroids/AsteroidGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour
+{
+
+    [SerializeField] GameObject asteroidPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GenAst",1f,4f);
+    }
+
+
+    public void GenAst()
+    {
+        Instantiate(asteroidPrefab, new Vector3(-5,Random.Range(-1,2),0) , Quaternion.identity);
+    }
+
+}
