457dcf9abd597cd3f22031d0b5db00d3a00edfe9
Francesco Avitabile <78053731+55hp@users.noreply.github.com>
2021-07-27 21:24:45 +0200

    Responsive wall

--- Assets/0. Game/LevelManager.cs
+++ Assets/0. Game/LevelManager.cs
@@ -6,9 +6,29 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField] MobManager mobManager;
 
+    [SerializeField] GameObject rightWall;
+    [SerializeField] GameObject leftWall;
+    [SerializeField] GameObject topWall;
+
+
+
+    public void SetWalls()
+    {
+
+        Debug.Log("" + Camera.main.orthographicSize);
+        Debug.Log("" + Camera.main.aspect);
+
+        topWall.transform.position = new Vector3(0 ,Camera.main.orthographicSize + 0.5f, 0);
+
+        rightWall.transform.position = new Vector3(Camera.main.orthographicSize * Camera.main.aspect + 0.5f , 0 , 0);
+        leftWall.transform.position = new Vector3(-Camera.main.orthographicSize * Camera.main.aspect - 0.5f , 0 , 0);
+    }
+
+
     private void OnEnable()
     {
         EventManager.OnStateHaveBeenChanged += OnStateChanged;
+        SetWalls();
     }
 
     private void OnDisable()
