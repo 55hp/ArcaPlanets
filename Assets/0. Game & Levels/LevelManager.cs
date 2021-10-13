15dcc209a3d39ca6ff4de2f05a4806740e736e64
Francesco Avitabile <78053731+55hp@users.noreply.github.com>
2021-10-13 17:07:13 +0200

    Planet activated

--- Assets/0. Game & Levels/LevelManager.cs
+++ Assets/0. Game & Levels/LevelManager.cs
@@ -66,11 +66,11 @@ public class LevelManager : Singleton<LevelManager>
 
     public void SetMobs(int satelliteNumber)
     {
-        /*
+        
         planet.SetPlanet(30, Ut.ROA(planetBodies),Ut.ROA(planetFaces), Ut.ROA(planetBodyPalette), Planet.PLANET_POWER.SINGLE_SHOT, 10, 8f, 4f , bulletPrefabs[0]);
         planet.gameObject.SetActive(true);
         planet.ActivatePlanet();
-        */
+        
         
         satellities[0].SetSatellite(10, Ut.ROA(satelliteBodies), Ut.ROA(satelliteFaces), Ut.ROA(satelliteBodyPalette), Satellite.SATELLITE_POWER.CANNON, 10, 5, 5, bulletPrefabs[1]);
         satellities[0].gameObject.SetActive(true);
